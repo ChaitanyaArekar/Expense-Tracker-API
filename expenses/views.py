8249a051726a64878e490705053ca4fa778cd368
Chaitanya Pradip Arekar <chaitanyaarekar32@gmail.com>
2026-07-01 14:37:29 +0530

    added analysis/summary details for expenses using custom actions

--- expenses/views.py
+++ expenses/views.py
@@ -5,6 +5,10 @@ from .serializers import CategorySerializer, ExpenseSerializer
 from .permissions import IsOwner  # Import the custom permission class
 from .filters import ExpenseFilter
 from .pagination import ExpensePagination
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from django.db.models import Sum, Count, Avg
+from django.db.models.functions import TruncMonth
 
 class CategoryViewSet(viewsets.ModelViewSet):
     serializer_class = CategorySerializer
@@ -30,3 +34,38 @@ class ExpenseViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)     # Automatically attach the logged-in user to user field on save
+
+    @action(detail=False, methods=['get'], url_path='monthly-summary')  # Define a custom action to get monthly summary of expenses
+    def monthly_summary(self, request):
+        queryset = self.get_queryset()
+        data = (
+            queryset
+            .annotate(month=TruncMonth('expense_date')) # Truncate the expense_date to month to group expenses by month
+            .values('month')
+            .annotate(total=Sum('amount'), count=Count('id'))
+            .order_by('month')
+        )
+        return Response(data)
+
+    @action(detail=False, methods=['get'], url_path='category-summary')  # Define a custom action to get category summary of expenses
+    def category_summary(self, request):
+        queryset = self.get_queryset()
+        data = (
+            queryset
+            .values('category__name')
+            .annotate(total=Sum('amount'), count=Count('id'), average=Avg('amount'))
+            .order_by('-total')
+        )
+        return Response(data)
+    
+    @action(detail=False, methods=["get"], url_path="top-spending-categories")  # Define a custom action to get top spending categories of expenses
+    def top_spending_categories(self, request):
+        limit = int(request.query_params.get("limit", 5))   # Get the limit from query parameters, default to 5 if not provided
+        queryset = self.get_queryset()
+        data = (
+            queryset
+            .values("category__id", "category__name")
+            .annotate(total=Sum("amount"))
+            .order_by("-total")[:limit]
+        )
+        return Response(data)
